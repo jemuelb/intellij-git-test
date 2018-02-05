ac05e1ccfcd2dc8994cc5f07b4cc3089f03954b1
Jemuel Balangcod <jemuelb@gmail.com>
2018-02-04 16:33:30 -0800

    Updates to both the helloworld and mynewfunction modules.

--- helloworld.py
+++ helloworld.py
@@ -4,3 +4,7 @@ print("This is my first file in this repository!")
 ##############################
 
 print("This is my second update for helloworld.")
+
+##############################
+
+print("This is a second update for helloworld.")
--- mynewfunction.py
+++ mynewfunction.py
@@ -1,2 +1,5 @@
 def doubler(a):
     return a * 2
+
+def halfer(a):
+    return a / 2
